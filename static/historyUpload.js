253378372ae7f680ed4f6c463fb96bdf3de1bed1
chrisw <chrisw@soe.ucsc.edu>
2013-12-06 16:28:17 -0800

    working on upload

--- static/historyUpload.js
+++ static/historyUpload.js
@@ -1,48 +1,47 @@
 // Upload a file to a Galaxy user's history using Galaxy's upload API.
 
-var UPLOAD_API_URL = 'api/tools';
+function uploadToGalaxyHistory(fileText, fileName, hdaJSObject) {
 
-function setupUpload() {
-    var currentId = parent.Galaxy.currHistoryPanel.model.get('id');
-    console.log('current id: ' + currentId);
-}
+    var UPLOAD_API_URL = 'api/tools';
+    var historyId = parent.Galaxy.currHistoryPanel.model.get('id');
 
-/**
- * some metadata for the upload
- */
-function configureUpload() {
     // configure tool
     tool_input = {};
     tool_input['dbkey'] = genome;
     tool_input['file_type'] = file_type;
-    tool_input['files_0|NAME'] = file.name;
+    tool_input['files_0|NAME'] = fileName;
     tool_input['files_0|type'] = 'upload_dataset';
     tool_input['files_0|url_paste'] = url_paste;
     tool_input['space_to_tabs'] = space_to_tabs;
 
     // setup data
     data = {};
-    data['history_id'] = this.current_history;
+    data['history_id'] = historyId;
     data['tool_id'] = 'upload1';
     data['inputs'] = JSON.stringify(tool_input);
-}
 
-/**
- * file gets added to a FormData object.
- */
-function constructUploadForm() {
     // construct form data
     var formData = new FormData();
-    for (var key in data)
-    formData.append(key, data[key]);
+    for (var key in data) {
+        formData.append(key, data[key]);
+    }
 
     // add file to form
-    formData.append('files_0|file_data', file, file.name);
-}
 
-/**
- * form object has to be posted to api/tools by calling/copying the send() function from static/scripts/utils/galaxy.uploadbox.js
- */
-function sendUploadForm() {
+    var fileBlob = new Blob([fileText], {
+        type : "text/plain"
+    });
 
+    formData.append('files_0|file_data', fileBlob, fileName);
+
+    // form object has to be posted to api/tools by calling/copying the send() function from static/scripts/utils/galaxy.uploadbox.js
+    var jqxhr = $.post(UPLOAD_API_URL, formData, function() {
+        alert("success");
+    }).done(function() {
+        alert("second success");
+    }).fail(function() {
+        alert("error");
+    }).always(function() {
+        alert("finished");
+    });
 }
